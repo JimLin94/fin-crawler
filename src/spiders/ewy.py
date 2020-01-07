c6f4025136d4ae8e78ab2850452b9c6e1c454610
Jim Lin <jimlin7694@gmail.com>
2020-01-08 02:36:02 +0800

    [ewy] Get the table of stock data by the client-side script

--- src/spiders/ewy.py
+++ src/spiders/ewy.py
@@ -5,15 +5,18 @@ from configs.url import KRX
 def ewy_spider():
     browser = ClientSideCrawler(KRX)
     driver = browser.driver
-
+    # Click the Stock id
     ewy_btn = driver.find_element_by_css_selector('input.schdate[value=STK]')
     ewy_btn.click()
     print('The radio button is clicked %s' % ewy_btn)
+    # Wait for the fetching data and get the HTML
+    browser.wait_for_element('tbody.CI-GRID-BODY-TABLE-TBODY')
+    table = driver.execute_script('''
+        var table = document.querySelector('tbody.CI-GRID-BODY-TABLE-TBODY');
 
-    table = browser.wait_for_element('tbody.CI-GRID-BODY-TABLE-TBODY')
+        return table.innerHTML;
+    ''')
     print('The table founded %s' % table)
-
-    print('The table content is %s' % table.get_attribute('innerHTML'))
 
     submit_btn = driver.find_element_by_css_selector('button.btn-board-search')
     submit_btn.click()
