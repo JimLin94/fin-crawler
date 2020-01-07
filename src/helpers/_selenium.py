882b4b5ab666049d72941f6345c94e79941abb64
Jim Lin <jiml@cnyes.com>
2020-01-07 16:14:54 +0800

    [setup] Change the driver to Chrome

--- src/helpers/_selenium.py
+++ src/helpers/_selenium.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 class ClientSideCrawler:
     def __init__(self, url):
-        self.driver = webdriver.Firefox()
-        firefox_options = Options()
-        firefox_options.add_argument('--headless')
-        firefox_options.add_argument('--disable-gpu')
-        firefox_options.add_argument('--hide-scrollbars')  # 隱藏滾動條, 應對一些特殊頁面
-        firefox_options.add_argument(
-            'blink-settings=imagesEnabled=false')  # 不載入圖片, 提升速度
-        # 瀏覽器不提供視覺化頁面. linux下如果系統不支援視覺化不加這條會啟動失敗
-        self.driver.get(url)
+        self.driver = webdriver.Chrome()
+        options = Options()
+        options.headless = True
+        # driver = webdriver.Chrome(chrome_options=options, executable_path='/path/to/chromedriver')
 
     def wait(self, delay = 5):
         self.driver.implicitly_wait(delay)
