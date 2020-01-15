5ff50ffdb0b9dba1b17b4b0099765f5ee847c985
Jim Lin <jimlin7694@gmail.com>
2020-01-15 23:37:16 +0800

    [n225] Add the appending time to data

--- src/main.py
+++ src/main.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
+import schedule
+import time
 import sys
 from packages import kospi, n225
 
 spider_name = sys.argv[1]
 
+
 def switcher():
+    print('Launch the job...')
+
     switch = {
         'kospi': kospi.kospi_spider,
         'n225': n225.n225_spider
     }
-    fn = switch.get(spider_name, "nothing")
 
-    return fn()
+    if spider_name:
+        fn = switch.get(spider_name, "The spider name not found.")
+        fn()
+    else:
+        schedule.every(5).seconds.do(fn)
+
+        while True:
+            schedule.run_pending()
+
+            time.sleep(1)
 
 def main():
     switcher()
+
 
 if __name__ == '__main__':
     main()
--- src/packages/n225.py
+++ src/packages/n225.py
@@ -2,10 +2,12 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+import re
 from configs.url import N225
 from helpers._pd import parse_html_to_excel
 
 import pandas as pd
+
 
 def n225_spider():
     res = requests.get(N225)
@@ -19,20 +21,23 @@ def n225_spider():
         today = datetime.strftime(datetime.now(), '%Y%m%d')
         source = []
 
-        print('Updated %s' % updated_time[)
-
         try:
-            updated_time_text = updated_time.get_text()
-            # updated_timestamp =
+            raw_updated_time_text = updated_time[0].get_text()
+            updated_time_text = re.sub(r'^Update：', '', raw_updated_time_text)
+            updated_time_form_instance = datetime.strptime('Jan/15/2020', '%b/%d/%Y')
+            updated_time_form_timestamp = updated_time_form_instance.timestamp()
+            updated_time_form_text = updated_time_form_instance.strftime('%Y/%m/%d')
+
+            print('Updated %s' % updated_time_form_instance.timestamp())
 
             for idx in range(len(code_list)):
-                source.append([code_list[idx].get_text(),
-                company_list[idx].get_text()], )
+                source.append([code_list[idx].get_text(), company_list[idx].get_text(
+                ), updated_time_form_text, updated_time_form_timestamp])
         except ValueError:
             print('Parse the DOM error')
 
         # print('HTML %s %s' % (code_list, company_list))
-    df = pd.DataFrame(source, columns=['Code', 'Company'])
+    df = pd.DataFrame(source, columns=['Code', 'Company', 'Date', 'Timestamp'])
 
     print('DF %s' % df)
     parse_html_to_excel(df, 'n225.xlsx', today)
