f5e823c64975572f5b97049f509393d8a1ba289f
Jim Lin <jiml@cnyes.com>
2020-02-05 13:21:32 +0800

    [n225] Work on the converter

--- src/converter.py
+++ src/converter.py
@@ -1,5 +1,7 @@
 from helpers._mysql import df_insert_to_db
 import pandas as pd
+import re
+from datetime import datetime
 import os
 
 from configs.url import N225
@@ -12,8 +14,17 @@ def convert_csv_to_sql():
     df = pd.DataFrame()
 
     for csv in files:
+        file_created_date = re.match(r'^2020(\d+)', csv)
+        date_format = datetime.strptime(file_created_date.group(), '%Y%m%d')
+        db_format_date = date_format.strftime('%Y/%m/%d')
         d = pd.read_excel(file_path + '/' + csv)
-        print('File name %s' % csv)
+        print('File date %s' % db_format_date)
+
+        if 'Date' not in d:
+            d['Date'] = db_format_date
+        if d['Date'].astype('str').str.contains(db_format_date) > 0:
+            print("Data has been added %s" % db_format_date)
+
         print(d)
         df = df.append(d, ignore_index=True)
 
