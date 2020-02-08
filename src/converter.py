618b32ad18ad0f109260544ae41aef38e2a160a7
Jim Lin <jimlin7694@gmail.com>
2020-02-08 17:41:54 +0800

    [n225] Create a backup data converter for N225

--- src/converter.py
+++ src/converter.py
@@ -14,6 +14,17 @@ def convert_csv_to_sql():
     df = pd.DataFrame()
 
     for csv in files:
+        d = pd.read_excel(file_path + '/' + csv)
+        df = df.append(d, ignore_index=True)
+
+    df_insert_to_db('n225', df, '')
+    print(df)
+
+def convert_csv_to_sql_archive():
+    files = [f for f in os.listdir(file_path) if '.xlsx' in f]
+    df = pd.DataFrame()
+
+    for csv in files:
         file_created_date = re.match(r'^2020(\d+)', csv)
         date_format = datetime.strptime(file_created_date.group(), '%Y%m%d')
         db_format_date = date_format.strftime('%Y/%m/%d')
@@ -22,14 +33,16 @@ def convert_csv_to_sql():
 
         if 'Date' not in d:
             d['Date'] = db_format_date
-        if d['Date'].astype('str').str.contains(db_format_date) > 0:
-            print("Data has been added %s" % db_format_date)
-
-        print(d)
-        df = df.append(d, ignore_index=True)
+        if d['Date'].iloc[0] != db_format_date:
+            print('Not matched %s %s' % (db_format_date, d['Date'].iloc[0]))
+        if d['Date'].iloc[0] == db_format_date:
+            df = df.append(d, ignore_index=True)
+            print('Appending the DF %s, %s' %
+                  (db_format_date, d['Date'].iloc[0]))
 
     db = df.drop(['Timestamp'], axis=1)
-    # df_insert_to_db('n225', db, '')
+    df_insert_to_db('n225', db, '')
+
     print(db)
 
 if __name__ == '__main__':
